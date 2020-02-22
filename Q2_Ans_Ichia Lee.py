9d48526f24dc964aff79e588f2aea7c093dbfd16
iclee11 <49114357+iclee11@users.noreply.github.com>
2020-02-21 23:02:38 -0800

    Update Q2_Ans_Ichia Lee.py

--- Q2_Ans_Ichia Lee.py
+++ Q2_Ans_Ichia Lee.py
@@ -13,19 +13,17 @@ from sklearn.metrics import r2_score
 df=pd.read_csv('Adops & Data Scientist Sample Data - Q2 Regression.csv')
 
 # setting the matrixes
-x = df.iloc[:,0:2].values 
-y = df.iloc[:,-1].values
+x, y = df.iloc[:,0:2].values, df.iloc[:,-1].values 
 # add constant term for linear regression
-x=np.concatenate((np.ones([x.shape[0],1]),x),axis=1)
+x = np.concatenate((np.ones((len(x),1)),x),axis=1)
 
 # Linear Regression using Gradient Descent 
 def LinearRegression(x,y,theta = np.ones((1,3)),iters = 2000,alpha = 0.02):
     for i in range(iters):
-        theta=theta-(alpha/len(x))*np.sum(x* 
-                (np.dot(x,theta.T)-y.reshape((-1,1))), axis=0)
+        theta=theta-(alpha/len(x))*np.sum(x*(np.dot(x,theta.T)-y.reshape((-1,1))), axis=0)
     return theta.reshape((-1,))
 
-#running the Gradient Descent and cost function
+# fit data
 theta=LinearRegression(x,y)
 print('fitted formula: y = %+.2f%+.2f*x1%+2f*x2' % tuple(theta))
 print('R2 score:',r2_score(y, np.dot(x, theta.T)))
