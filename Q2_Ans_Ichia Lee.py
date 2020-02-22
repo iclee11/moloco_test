c8e05475cbb32bbc83e312d2403bf7d5dcc62353
iclee11 <49114357+iclee11@users.noreply.github.com>
2020-02-21 22:47:17 -0800

    Update Q2_Ans_Ichia Lee.py

--- Q2_Ans_Ichia Lee.py
+++ Q2_Ans_Ichia Lee.py
@@ -1,41 +1,37 @@
+'''
+Using linear regression to fit the data. The R^2 is pretty descent, 0.006!
+
+* Fiting results: 
+fitted formula: y = -19.29-8.07*x1-1.778012*x2
+R2 score: 0.0060532841614568955
+'''
+
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 df=pd.read_csv('Adops & Data Scientist Sample Data - Q2 Regression.csv', 
                names=['A','B','C'])
-
-#normalize the data
-df=(df-df.mean())/df.std()
 
 #setting the matrixes
 x=df.loc[:,['A','B']]
 ones = np.ones([x.shape[0],1])
 x=np.concatenate((ones,x),axis=1)
-
 y=df.loc[:,'C'].values #converts it from pandas DataFrame to numpy array
-theta = np.zeros([1,3])
 
 #set hyperparameters
-alpha=0.02
-iters=2000
+alpha=0.02 # learning rate in gradient descent
+iters=2000 # num. of iterations
+theta = np.random.rand(1,3) # initial guess of the parameters
 
 #create the Gradient Descent 
-def Gradient_Descent(x,y,theta,iters,alpha):
+def Linear_Regression(x,y,theta,iters,alpha):
     for i in range(iters):
         theta=theta-(alpha/len(x))*np.sum(x* 
-                        (np.dot(x,theta.T)-y.reshape((-1,1))), axis=0)
-    return theta
+                (np.dot(x,theta.T)-y.reshape((-1,1))), axis=0)
+    return theta.reshape((-1,))
 
 #running the Gradient Descent and cost function
-result=Gradient_Descent(x,y,theta,iters,alpha)
-print('result:', result)
-
-#Validate by sklearn
-from sklearn.linear_model import LinearRegression
-model=LinearRegression()
-x_model=df[['A','B']]
-y_model=df['C']
-model.fit(x_model,y_model)
-print('intercept:',model.intercept_)
-print('coefficient:',model.coef_)
-
+theta_fitted=Linear_Regression(x,y,theta,iters,alpha)
+print('fitted formula: y = %+.2f%+.2f*x1%+2f*x2' % tuple(theta_fitted))
+print('R2 score:',r2_score(y, np.dot(x, theta_fitted.T)))
